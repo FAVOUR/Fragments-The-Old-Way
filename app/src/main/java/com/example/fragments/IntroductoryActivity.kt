9d98f1a130fdd66398c8808335c6adb6e773c400
FAVOUR <truelyfavoured@gmail.com>
2020-07-24 07:51:50 +0100

    Done with first page for fragment navigation

---
+++ app/src/main/java/com/example/fragments/IntroductoryActivity.kt
@@ -0,0 +1,27 @@
+package com.example.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import com.example.fragments.ExploreFragment.Companion.newInstance
+
+class IntroductoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empty)
+
+        setupFragment()
+    }
+
+
+    fun setupFragment() {
+            val exploreFragment = newInstance()
+            val fragmentManager: FragmentManager = supportFragmentManager
+            fragmentManager.beginTransaction().apply {
+                                                        add(R.id.container,exploreFragment)
+                                                        addToBackStack(ExploreFragment::class.simpleName)
+                                                         commit()
+                                                         }
+    }
+}
